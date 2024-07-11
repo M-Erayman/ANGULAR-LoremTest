21399dc91a001eae51dc23931af93314772baa2c
M-Erayman <muhammedderayman@gmail.com>
2024-07-11 17:03:17 +0300

    buyuk-kucuk harf yapildii

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./navbar/navbar.component";
-import { MiddleComponent } from "./middle/middle.component";
-import { BottomComponent } from "./bottom/bottom.component";
+import { NavbarComponent } from './navbar/navbar.component';
+import { MiddleComponent } from './middle/middle.component';
+import { BottomComponent } from './bottom/bottom.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, MiddleComponent, BottomComponent],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    MiddleComponent,
+    BottomComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'HarfCSS';
--- src/app/middle/middle.component.ts
+++ src/app/middle/middle.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
-
+import { faker } from '@faker-js/faker';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'middleComponent',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './middle.component.html',
-  styleUrl: './middle.component.css'
+  styleUrl: './middle.component.css',
 })
 export class MiddleComponent {
+  title = 'Harf';
+  randomText = faker.lorem.sentence();
+  enteredText = '';
 
+  onClick() {
+    // window.location.reload();
+    this.randomText = faker.lorem.sentence();
+  }
+
+  getInputValue(value: string) {
+    this.enteredText = value;
+    console.log(this.enteredText);
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending';
+    } else if (randomLetter === enteredLetter) {
+      //  (randomLetter = randomLetter.toLowerCase()) ===
+      // enteredLetter = enteredLetter.toLowerCase()
+      return 'correct';
+    } else {
+      return 'incorrect';
+    }
+  }
 }
