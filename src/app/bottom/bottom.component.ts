d0eada4613b735d790dbd2a94fcdc73782c6648a
M-Erayman <muhammedderayman@gmail.com>
2024-07-11 17:26:17 +0300

    checkbox kontrolleri yapıldı

--- src/app/bottom/bottom.component.ts
+++ src/app/bottom/bottom.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'bottomComponent',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './bottom.component.html',
   styleUrl: './bottom.component.css',
 })
-export class BottomComponent {}
+export class BottomComponent {
+  cb1Ischecked: boolean = false;
+  cb2Ischecked: boolean = false;
+  cb3Ischecked: boolean = false;
+  cbList: boolean[] = [];
+
+  cb1Control() {
+    // console.log(this.cb1Ischecked);
+    this.cbList[0] = this.cb1Ischecked;
+    console.log(this.cbList);
+  }
+  cb2Control() {
+    // console.log(this.cb1Ischecked);
+    this.cbList[1] = this.cb2Ischecked;
+    console.log(this.cbList);
+  }
+  cb3Control() {
+    // console.log(this.cb1Ischecked);
+    this.cbList[2] = this.cb3Ischecked;
+    console.log(this.cbList);
+  }
+}
