426cf2586c760bfb37ce318ef7242fce75deadbd
M-Erayman <muhammedderayman@gmail.com>
2024-07-17 11:37:14 +0300

    options header added. Enter keydown event added. Upper case function added and case sensitivity button fixed

--- src/app/bottom/bottom.component.ts
+++ src/app/bottom/bottom.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BaseService } from '../service/base.service';
 
+
 @Component({
   selector: 'bottomComponent',
   standalone: true,
--- src/app/middle/middle.component.ts
+++ src/app/middle/middle.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
 import { faker } from '@faker-js/faker';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -13,26 +13,40 @@ import { FormsModule } from '@angular/forms';
 })
 export class MiddleComponent {
   title = 'Harf';
-  randomText = faker.lorem.sentence();
+  randomText = '';
   enteredText = '';
   isZoomed: boolean = false;
   list: boolean[] = [];
-  beforeZoom: string = 'Please chose your option and start the game...';
+  beforeZoom: string = 'Please choose your option and start the game...';
   afterZoom: string = 'Please enter the text...';
+  enterWatcher: boolean = false;
 
-  constructor(private baseService: BaseService) {}
+  constructor(private baseService: BaseService) {
+    this.onClick();
+  }
 
   // Sayfa baslangicinda calisir...
   ngOnInit() {
     this.baseService.onDataChange().subscribe((data: any) => {
-      console.log(data);
+      //console.log(data);
       this.list = data;
     });
+    this.onClick;
+  }
+
+  enterKeydown() {
+    this.onClick();
   }
 
   onClick() {
     // window.location.reload();
     this.randomText = faker.lorem.sentence();
+    this.randomText = this.randomText.slice(0, -1);
+    if (this.list[0]) {
+      console.log(this.randomText.length);
+      this.randomText = this.convertThirdToUpper(this.randomText); // Ucte birini buyuk harf yapar (GPT)...
+    }
+    console.log(this.enteredText);
     this.enteredText = '';
   }
 
@@ -55,5 +69,33 @@ export class MiddleComponent {
     } else {
       return 'incorrect';
     }
+  }
+
+  convertThirdToUpper(str: string) {
+    // String'i bir diziye dönüştür
+    let chars = str.split('');
+
+    // Harf olmayan karakterleri ayıkla ve sadece harfleri içeren bir dizi oluştur
+    let letters = chars.filter((char) => /[a-zA-Z]/.test(char));
+
+    // Üçte biri kadar harfi büyük harf yapmamız gerektiğini hesapla
+    let numToUpper = Math.ceil(letters.length / 3);
+
+    // Rastgele harfleri seçip büyük harfe çevir
+    for (let i = 0; i < numToUpper; i++) {
+      let index = Math.floor(Math.random() * letters.length);
+      letters[index] = letters[index].toUpperCase();
+    }
+
+    // Harf olmayan karakterleri yerinde bırakmak için orijinal diziyi güncelle
+    let letterIndex = 0;
+    for (let i = 0; i < chars.length; i++) {
+      if (/[a-zA-Z]/.test(chars[i])) {
+        chars[i] = letters[letterIndex];
+        letterIndex++;
+      }
+    }
+    // Diziyi tekrar string'e dönüştür
+    return chars.join('');
   }
 }
